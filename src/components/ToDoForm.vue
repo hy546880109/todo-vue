8d410f1996a051c802fb8da7b6f3dc596a328fd5
HY\harry <hy546880109@qq.com>
2023-02-16 10:26:48 +0800

    vue学习

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <label for="new-todo-input" class="label__lg">你需要添加的任务? </label>
+        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.trim.lazy="label"
+            class="input__lg" />
+        <button type="submit" class="btn btn__primary btn__lg">添加</button>
+    </form>
+</template>
+
+<script>
+export default {
+    methods: {
+        onSubmit() {
+            if (this.label === '') {
+                return;
+            }
+            console.log('label: ', this.label);
+            this.$emit("todo-added", this.label);
+            this.label = "";
+        },
+    },
+    data() {
+        return {
+            label: "",
+        };
+    },
+};
+</script>
